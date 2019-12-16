d72c260aac69c3246c6ecd8ccebbe71fc30db5c7
gurgul <gurgul@d17-l427-pc-12.ipa.ki.agh.edu.pl>
2019-12-16 09:27:09 +0100

    kalkulator i mozliwe usprawnienia

--- calculator.php
+++ calculator.php
@@ -3,18 +3,18 @@ $op=$_GET['op'];
 $x=$_GET['x'];
 $y=$_GET['y'];
 if(($_GET['op'])=="sum"){
-	$eq=$_GET['x']+$_GET['y'];
-	echo "$x+$y=$eq";
+	$eq=($_GET['x']??0)+($_GET['y']??0);
+	echo "<h1>$x+$y=$eq</h1>";
 };
 if(($_GET['op'])=="subtract"){
-        $eq=$_GET['x']-$_GET['y'];
-        echo "$x-$y=$eq";
+        $eq=($_GET['x']??0)-($_GET['y']??0);
+        echo "<h1>$x-$y=$eq</h1>";
 };
 if(($_GET['op'])=="multiply"){
-        $eq=$_GET['x']*$_GET['y'];
-        echo "$x*$y=$eq";
+        $eq=($_GET['x']??1)*($_GET['y']??1);
+        echo "<h1>$x*$y=$eq</h1>";
 };
 if(($_GET['op'])=="divide"){
-        $eq=$_GET['x']/$_GET['y'];
-        echo "$x/$y=$eq";
+        $eq=($_GET['x']??1)/($_GET['y']??1);
+        echo "<h1>$x/$y=$eq<h1>";
 };
